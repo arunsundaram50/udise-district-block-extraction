4c7b47cd361dd09ee133388367f5e07fa512b2dd
arunsundaram50 <arun_co@yahoo.com>
2024-07-16 16:17:38 -0400

    updates

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ ap = argparse.ArgumentParser("UDISE district and block scrapper")
 ap.add_argument("-s", "--start_from", type=int, help="start from", required=False, default=0)
 ap.add_argument("-m", "--max", type=int, help="maximum rows to process", required=False, default=-1)
 ap.add_argument("-i", "--input", type=str, help="input filename", required=False, default="11DIstrict and Block updated - UDISE.xlsx")
-ap.add_argument("-a", "--attempts", type=int, help="max number of attempts when processing a row fails", required=False, default=5)
+ap.add_argument("-a", "--attempts", type=int, help="max number of attempts when processing a row fails", required=False, default=10)
 ap.add_argument("-w", "--wait", type=int, help="max number of seconds to wait for tables contents to appear after submitting the form", required=False, default=3)
+ap.add_argument("-t", "--trace_level", type=int, help="trace level", required=False, default=0)
 args = ap.parse_args()
 
 
@@ -19,6 +20,7 @@ INPUT_FILENAME = args.input
 OUTPUT_FILENAME = f"district_block_output_{START_FROM}-{START_FROM+MAX_ROWS_TO_PROCESS}.xlsx"
 SAVE_INTERMEDIATE_FILES = False
 ADD_PADDING = True
+TRACE_LEVEL = args.trace_level
 
 
 import os, subprocess, sys
@@ -37,7 +39,7 @@ if not SKIP_AUTO_VENV and os.getenv("VIRTUAL_ENV") != venv_path:
 
 
 from PIL import Image
-import pandas as pd
+import pandas as pd, time
 import os, pytesseract, io, base64, traceback, signal
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -45,6 +47,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 def get_captcha_text(udise_code):
@@ -120,7 +123,8 @@ def submit_form(udise_code, captcha_text):
   block_xpath = "//table[@id='example']/tbody/tr[1]/td[4]/text()[5]"
   state_management_xpath  ="substring-after(//b[text()='State Mgmt.']/following-sibling::text()[1], ':')"
   national_management_xpath = "substring-after(//b[text()='State Mgmt.']/following-sibling::text()[3], ': ')"
-  location_xpath = "substring-after(//b[text()='State Mgmt.']/following-sibling::text()[9], ': ')"
+  location_link_xpath = "//*[@class='clickLink']"
+  location_xpath = "//div[@class='row']/div[b/text()='Location: ']/following-sibling::div/span/text()"
 
   try:
     # Enter text into the search input field
@@ -140,9 +144,28 @@ def submit_form(udise_code, captcha_text):
     block = get_text(block_xpath)
     state_management = get_text(state_management_xpath)
     national_management = get_text(national_management_xpath)
+
+    location_link = driver.find_element(By.XPATH, location_link_xpath)
+    original_window = driver.current_window_handle
+    location_link.click()
+    # Wait for the new tab to open and switch to it
+    WebDriverWait(driver, 10).until(EC.number_of_windows_to_be(2))
+
+    for window_handle in driver.window_handles:
+      if window_handle != original_window:
+        driver.switch_to.window(window_handle)
+        break
+    
     location = get_text(location_xpath)
+
+    # Close the new tab
+    driver.close()
+    driver.switch_to.window(original_window)
+
     return district, block, state_management, national_management, location
-  except Exception:
+  except Exception as e:
+    if TRACE_LEVEL>=2:
+      traceback.print_exc()
     return None, None, None, None, None
 
 
@@ -167,7 +190,7 @@ def main():
     os.makedirs("captcha")
 
   df_input = pd.read_excel(INPUT_FILENAME)
-  home_url = "https://src.udiseplus.gov.in/home"
+  home_url = "https://src.udiseplus.gov.in/home" # example UDISE CODE: 07080317203
 
   for idx, (_, row) in enumerate(df_input.iterrows(), start=1):
     if idx<START_FROM:
@@ -180,7 +203,7 @@ def main():
     for attempt in range(1, MAX_ATTEMPTS_PER_ROW+1):
       driver.get(home_url)
       if attempt==1:
-        print(f'{idx:,} of {len(df_input):,}', end='')
+        print(f'{idx-START_FROM+1:,}): {idx:,} of {START_FROM} to {START_FROM+MAX_ROWS_TO_PROCESS:,} out of {len(df_input):,}', end='')
       else:
         print('.', end='')
       captcha_text = get_captcha_text(udise_code)
@@ -193,6 +216,8 @@ def main():
       elif attempt==MAX_ATTEMPTS_PER_ROW:
         results.append({"UDISE Code": udise_code, "District": district, "Block": block, "State Mgmt": state_management, "National Mgmt": national_management, "Location": location})
     print('')
+    if TRACE_LEVEL>=1:
+      print(district, block, state_management, national_management, location, sep=" | ")
 
   save_results()
 
@@ -202,7 +227,14 @@ if __name__ == "__main__":
   signal.signal(signal.SIGTERM, signal_handler)
   try:
     results = []
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920x1080")
+
+    # Initialize the WebDriver with the specified options
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
     main()
   except Exception as e:
     print(f"Unexpected error: {e}")
